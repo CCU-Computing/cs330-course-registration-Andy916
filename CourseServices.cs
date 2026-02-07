0418a1a18030fa9b9d6cdfd9c296a94c3566378f
Andy Hattabaugh <andyhattabaugh@gmail.com>
2026-02-07 16:11:01 -0500

    Finished user-story-3

--- CourseServices.cs
+++ CourseServices.cs
@@ -15,17 +15,32 @@ namespace cs330_proj1
           return null;
         }
 
+
         
         //Add more service functions here, as needed, for the project
 
         /* As a student, I want to see all available courses so that I know what my options are */
 
-        /* As a student, I want to see all course offerings by semester, so that I can choose from what's
-           available to register for next semester */
+        /* As a student, I want to see all course offerings by semester, so that I can choose from what's available to register for next semester */
+         public List<CourseOffering> getOfferingsBySemester(string semester)
+      {
+         List<CourseOffering> allOfferings = repo.Offerings;
+         List<CourseOffering> result = new List<CourseOffering>();
+
+         foreach (CourseOffering offering in allOfferings)
+         {
+            if (offering.Semester.Equals(semester))
+            {
+                  result.Add(offering);
+            }
+         }
+         return result;
+      }
+
 
         /* As a student I want to see all course offerings by semester and department so that I can 
         choose major courses to register for */
-
+ 
         /* As a student I want to see all courses that meet a core goal, so that I can plan out
            my courses over the next few semesters and choose core courses that make sense for me */
 
@@ -36,7 +51,5 @@ namespace cs330_proj1
         /* As a freshman adviser, I want to see all the core goals which do not have any course offerings 
            for a given semester, so that I can work with departments to get some courses offered
            that students can take to meet those goals */
-
-        
      }
 }
